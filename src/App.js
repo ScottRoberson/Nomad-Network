b58e79184e30454c56d52c30657b2e5bedc023b0
Scott Roberson <srkazan23@gmail.com>
2020-08-01 15:46:57 -0500

    added hero image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
       Website Coming Soon
+      <Hero/>
     </div>
   );
 }
---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import hero from './images/hero.jpg'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className="overlay">
+    <div className="two"></div>
+    
+    
+    </div> 
+  )
+}
+
+export default Hero
