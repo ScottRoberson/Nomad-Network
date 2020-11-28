1ba72e3382d72fa517f13e0d56286cab4144d204
Scott Roberson <srkazan23@gmail.com>
2020-11-28 15:16:18 -0600

    Added file upload to post modal.

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,17 +1,18 @@
 import React from 'react';
-
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-import PhotoIcon from '@material-ui/icons/Photo';
-import './CreatePost.css';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import './PostModal.css';
+import ModalAlert from './ModalAlert';
 
 const PostModal = ({ open, handleClose }) => {
   return (
     <div>
+      <ModalAlert />
       <Dialog open={open} aria-labelledby='form-dialog-title'>
         <IconButton
           aria-label='close'
@@ -27,8 +28,13 @@ const PostModal = ({ open, handleClose }) => {
               placeholder='What is on your mind?'
             />
           </DialogContent>
-          <PhotoIcon style={{ color: 'green' }} />
-          <input type='file' />
+          <div className='modal-upload-container'>
+            <input type='file' id='file' className='inputfile' />
+            <label for='file'>
+              <CameraAltIcon style={{ fontSize: '32px' }} />
+            </label>
+          </div>
+
           <div className='output'></div>
           <DialogActions>
             <button className='post-modal-btn'> Post</button>
