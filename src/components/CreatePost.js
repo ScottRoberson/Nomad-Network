bb08d58cf383bb2fac485a5560fb06d0a8b234fd
Scott Roberson <srkazan23@gmail.com>
2020-11-28 17:36:42 -0600

    added logic to post modal and added an alert

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -2,27 +2,54 @@ import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import lucky from '../images/Lucky.jpg';
 import PostModal from './PostModal';
-import ModalAlert from './ModalAlert';
+
 const CreatePost = () => {
   const [open, setOpen] = useState(false);
+  const [openModalAlert, setOpenModalAlert] = useState(false);
+  const [postText, setPostText] = useState('');
+  const handleModalAlertOpen = () => {
+    setOpenModalAlert(true);
+  };
 
-  const handleClickOpen = () => {
+  const handleModalAlertClose = () => {
+    setOpenModalAlert(false);
+  };
+
+  const handleModalOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = () => {
+  const handleModalClose = () => {
+    if (postText) {
+      handleModalAlertOpen();
+    } else {
+      setOpen(false);
+    }
+  };
+
+  const handleDiscardPost = () => {
     setOpen(false);
+    setPostText('');
+    handleModalAlertClose();
   };
 
   return (
     <div className='create-post-container'>
       <div className='create-post__inner-container'>
         <Avatar alt='lucky Sob' src={lucky} />
-        <button className='create-post-button' onClick={handleClickOpen}>
+        <button className='create-post-button' onClick={handleModalOpen}>
           Where in the world are you today?
         </button>
       </div>
-      <PostModal open={open} handleClose={handleClose} />
+      <PostModal
+        open={open}
+        postText={postText}
+        setPostText={setPostText}
+        openModalAlert={openModalAlert}
+        handleModalClose={handleModalClose}
+        handleDiscardPost={handleDiscardPost}
+        handleModalAlertClose={handleModalAlertClose}
+      />
 
       <hr className='divider'></hr>
     </div>
--- src/components/ModalAlert.js
+++ src/components/ModalAlert.js
@@ -2,46 +2,30 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-export default function ModalAlert() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+export default function ModalAlert({
+  openModalAlert,
+  handleDiscardPost,
+  handleModalAlertClose,
+}) {
   return (
     <div>
-      <Button variant='outlined' color='primary' onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
       <Dialog
-        open={open}
-        onClose={handleClose}
+        open={openModalAlert}
+        onClose={handleModalAlertClose}
         aria-labelledby='alert-dialog-title'
         aria-describedby='alert-dialog-description'>
-        <DialogTitle id='alert-dialog-title'>
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id='alert-dialog-description'>
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
+        <DialogTitle id='alert-dialog-title'>{'Discard Post'}</DialogTitle>
         <DialogActions>
-          <Button onClick={handleClose} color='primary'>
-            Disagree
+          <Button onClick={handleModalAlertClose} color='primary'>
+            Cancel
           </Button>
-          <Button onClick={handleClose} color='primary' autoFocus>
-            Agree
+          <Button
+            onClick={handleDiscardPost}
+            style={{ backgroundColor: '#1877f2', color: '#fff' }}
+            autoFocus>
+            Delete
           </Button>
         </DialogActions>
       </Dialog>
