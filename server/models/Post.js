05563b9c4cdd61cc5c56ef0f61f65efea6be57f6
Scott Roberson <srkazan23@gmail.com>
2020-12-20 20:55:53 -0600

    added delete, get by id, and fixed post route

--- server/models/Post.js
+++ server/models/Post.js
@@ -7,7 +7,6 @@ const postSchema = new mongoose.Schema({
   },
   postImage: {
     type: String,
-    required: true,
   },
   created: { type: Date, default: Date.now },
 });
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -16,21 +16,53 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route    GET api/posts/:id
+// @desc     Get a post by ID
+// @access   TBD
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+
+    res.status(500).send('Server Error');
+  }
+});
+
 // @route    POST api/posts
 // @desc     Create a post
 // @access   TBD
 router.post('/', upload.single('postImage'), async (req, res) => {
-  const post = new Post({
-    postText: req.body.postText,
-    postImage: req.file.path,
-  });
-  console.log(req.file);
-
+  const post = new Post(req.body);
+  if (req.file !== undefined) {
+    post.postImage = req.file.filename;
+  }
   try {
     const createdPost = await post.save();
     res.status(201).json(createdPost);
   } catch (error) {
     res.status(400).json({ message: error });
+  }
+});
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   TBD
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    await post.remove();
+    res.json({ message: 'Post removed' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
   }
 });
 
