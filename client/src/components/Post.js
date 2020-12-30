3d0d31d9bbc886cc69cb5f30acc6b7964b9bcf49
Scott Roberson <srkazan23@gmail.com>
2020-12-29 19:35:14 -0600

    finished newsfeed post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import './Post.css';
-import w from '../images/wrigley-field.jpg';
 import FeedProfile from './FeedProfile';
 import CommentBox from './CommentBox';
 
 function Post({ posts }) {
   const postslist = posts.map((post) => {
     return (
-      <div className='post'>
-        <p>{post.postText}</p>
-        <img src={post.postImage} alt='user upload' className='post-image' />
+      <div>
+        <FeedProfile />
+        <div className='post__container'>
+          <div className='post'>
+            <p>{post.postText}</p>
+            <img src={post.postImage} alt='' className='post-image' />
+          </div>
+          <CommentBox />
+        </div>
       </div>
     );
   });
 
-  return (
-    <div className='post-parent'>
-      <FeedProfile />
-      <div className='post__container'>
-        {postslist}
-        <CommentBox />
-      </div>
-    </div>
-  );
+  return <div className='post-parent'>{postslist}</div>;
 }
 
 export default Post;
