f06a4ee1948abfbd7bf72674f624a231fa90fe5c
Scott Roberson <srkazan23@gmail.com>
2021-01-09 20:43:09 -0600

    added redux & created store, rootreducer and action folder

---
+++ client/src/redux/reducers/postReducers.js
@@ -0,0 +1,26 @@
+import { GET_POST, POST_LOADING } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  loading: false,
+};
+
+const post = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false,
+      };
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default post;
