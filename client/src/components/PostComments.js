09de961196091fd24738d8d59edf0f819c0405a5
Scott Roberson <srkazan23@gmail.com>
2021-03-22 19:55:20 -0500

    finished css and html for delete comment functionality

--- client/src/components/PostComments.js
+++ client/src/components/PostComments.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { makeStyles } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import DeleteIcon from '@material-ui/icons/Delete';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Dialog from '@material-ui/core/Dialog';
 import FeedProfile from './FeedProfile';
 import './PostComments.css';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+  dialog: {
+    position: 'absolute',
+    top: 205,
+  },
+  list: {
+    width: 475,
+  },
+  listItem: {
+    '&:hover': {
+      backgroundColor: '#e5e5e5 !important',
+    },
+  },
+});
+
 const PostComments = ({ comments }) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
   const commentlist = comments.map((comment) => {
     return (
       <div className='post-comments'>
+        <div className='more-container'>
+          <Button onClick={handleClickOpen}>
+            <MoreHorizIcon />
+          </Button>
+          <Dialog open={open}>
+            <List className={classes.list}>
+              <ListItem className={classes.listItem}>
+                <Avatar className={classes.avatar}>
+                  <DeleteIcon />
+                </Avatar>
+                <ListItemText primary='Delete' />
+              </ListItem>
+            </List>
+          </Dialog>
+        </div>
         <FeedProfile />
         <div className='comment-text-container' key={comment._id}>
           <p className='comment-text'>{comment.text}</p>
